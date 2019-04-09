8472031254a218a965e2b17f00a018cff464b671
2oops <yxlaimee@163.com>
2019-04-09 17:38:43 +0800

    init

---
+++ src/components/device-stat.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Description: device statistics设备统计
+ * @Author: 2oops
+ * @Date: 2019-04-09
+ -->
+
+<template>
+  <div class="main">
+    hello grid
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'deviceStat',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.main {
+
+}
+</style>
